66ef9333cb3033fe7265fbbf67aa0ade4b715bd8
soumyamehta21 <soumyamehta21@gmail.com>
2026-03-12 17:27:13 +0530

    Dummy commit

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,8 @@ import "./index.css";
 import "./i18n";
 import App from "./App";
 
+// dummy commit
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Provider store={store}>
